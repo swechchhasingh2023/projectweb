df25566d80708d4eea096d06be2f5e626e639190
Swechchha Singh <swechchhasingh2023@gmail.com>
2024-07-28 23:06:07 +0530

    code

---
+++ data/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const uri = 'mongodb+srv://swechchhasingh2017:3AygzMS5TnB5JX76@mern.mjg31yk.mongodb.net/?retryWrites=true&w=majority&appName=mern';
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(3005, () => {
+      console.log('Server has started on port 3004');
+    });
+  })
+  .catch(err => {
+    console.log('MongoDB connection error:', err);
+  });
+
+
+
+
+
+
+
+
+
+
+
